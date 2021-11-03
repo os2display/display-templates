fffe9e53b81cc0ad7f844e26803374ab92f08473
Sine <sinje@aarhus.dk>
2021-11-03 09:29:35 +0100

    book review test

---
+++ src/book-review/book-review.spec.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { shallow, configure } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import BookReview from "./book-review";
+import slides from "../../examples/src/slides";
+
+configure({ adapter: new Adapter() });
+
+test("test that app loads", () => {
+  const slide = slides[1];
+  const wrapper = shallow(
+    <BookReview
+      run
+      slide={slide}
+      content={slide.content}
+      slideDone={() => { }}
+    />
+  );
+
+  expect(
+    wrapper.find(".image-blurry-background").get(0).props.style.backgroundImage
+  ).toBe('url("./fixtures/images/mountain1.jpeg")');
+  expect(
+    wrapper.find(".author-image").get(0).props.style.backgroundImage
+  ).toBe('url("./fixtures/images/author.jpg")');
+  expect(
+    wrapper.find("h1").text()).toContain("Lorem Ipsum")
+  expect(
+    wrapper.find(".author").text()).toContain("There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.")
+});
