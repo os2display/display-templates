85f2220d3215f6636a1994d78557795635ed3fa0
Sine <sinje@aarhus.dk>
2021-12-15 14:37:59 +0100

    AR-705: added default value to contacts

--- src/contacts/contacts.js
+++ src/contacts/contacts.js
@@ -17,7 +17,7 @@ import { ThemeStyles } from "../slide-util";
  * @returns {object} The component.
  */
 function Contacts({ slide, content, run, slideDone }) {
-  const { contacts } = content;
+  const { contacts } = content ||[];
   const [translations, setTranslations] = useState();
   const { separator } = content.styling || {};
 
@@ -67,7 +67,8 @@ function Contacts({ slide, content, run, slideDone }) {
                 <div>{contact.phone}</div>
               </div>
             </div>
-          ))}
+          ))
+          }
         </div>
       </div>
     </IntlProvider>
