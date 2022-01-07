1fa98ae4da918bbf444752f1bdbb1b48585d2aa5
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-01-07 08:15:26 +0100

    AR-551: Fixed calendar layouts

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -4,12 +4,10 @@ import dayjs from "dayjs";
 import localeDa from "dayjs/locale/da";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import { IntlProvider, FormattedMessage } from "react-intl";
-import parse from "html-react-parser";
-import DOMPurify from "dompurify";
 import BaseSlideExecution from "../base-slide-execution";
 import "./calendar.scss";
 import da from "./lang/da.json";
-import { ThemeStyles } from "../slide-util";
+import { getFirstMediaUrlFromField, ThemeStyles } from "../slide-util";
 
 /**
  * Calendar component.
@@ -25,10 +23,35 @@ function Calendar({ slide, content, run, slideDone }) {
   const [currentDate, setCurrentDate] = useState(null);
   const [translations, setTranslations] = useState();
 
-  const { backgroundColor, hasDateAndTime, title } = content;
+  const {
+    backgroundColor = "",
+    hasDateAndTime,
+    title,
+    displayHeaders = true,
+    layout = "multiple",
+    footerText = null,
+    subTitle = null,
+    resourceAvailableText = null,
+    colorize = false,
+    colorizeColorClass = "",
+    resourceUnavailableText = null,
+  } = content;
   const { feedData = [] } = slide;
 
-  const classes = `template-calendar ${backgroundColor}`;
+  const classes = ["template-calendar"];
+  const rootStyle = {};
+
+  const imageUrl = getFirstMediaUrlFromField(slide.mediaData, content.image);
+
+  if (imageUrl) {
+    rootStyle.backgroundImage = `url("${imageUrl}")`;
+  }
+
+  if (colorize && colorizeColorClass !== "") {
+    classes.push("colorize", colorizeColorClass);
+  } else {
+    rootStyle.backgroundColor = backgroundColor;
+  }
 
   /** Setup slide run function. */
   const slideExecution = new BaseSlideExecution(slide, slideDone);
@@ -42,22 +65,26 @@ function Calendar({ slide, content, run, slideDone }) {
     };
   }, [run]);
 
-  /** Imports language strings, sets localized formats and sets timer. */
+  /** Imports language strings, sets localized formats. */
   useEffect(() => {
     dayjs.extend(localizedFormat);
 
     setTranslations(da);
+  }, []);
 
-    let timer = null;
+  useEffect(() => {
+    let dateAndTimeInterval = null;
+
     if (hasDateAndTime) {
-      timer = setInterval(() => setCurrentDate(new Date()), 1000);
+      dateAndTimeInterval = setInterval(() => setCurrentDate(new Date()), 1000);
     }
+
     return function cleanup() {
-      if (timer !== null) {
-        clearInterval(timer);
+      if (dateAndTimeInterval !== null) {
+        clearInterval(dateAndTimeInterval);
       }
     };
-  }, []);
+  }, [hasDateAndTime]);
 
   // Sort events by datetime and filter away events that are done.
   const getSortedEvents = (data) => {
@@ -83,43 +110,165 @@ function Calendar({ slide, content, run, slideDone }) {
     return s.charAt(0).toUpperCase() + s.slice(1);
   };
 
+  const groupEventsByDays = (events) => {
+    const days = {};
+
+    events.forEach((event) => {
+      const dayString = dayjs(event.startTime * 1000)
+        .locale(localeDa)
+        .format("dddd D. MMMM");
+
+      if (!Object.prototype.hasOwnProperty.call(days, dayString)) {
+        days[dayString] = [];
+      }
+
+      days[dayString].push(event);
+    });
+
+    return days;
+  };
+
+  const renderTimeOfDay = (unixTimestamp) => {
+    return dayjs(unixTimestamp * 1000)
+      .locale(localeDa)
+      .format("HH:mm");
+  };
+
+  const renderDays = (days) => {
+    return Object.keys(days).map((dayString, index) => (
+      <Fragment key={dayString}>
+        {index < 5 && (
+          <section className="multiple-days--group">
+            <h3 className="multiple-days--group-header">{dayString}</h3>
+            {days[dayString].map((event) => (
+              <article key={event.id} className="multiple-days--group-item">
+                <div>
+                  {renderTimeOfDay(event.startTime)} -{" "}
+                  {renderTimeOfDay(event.endTime)}
+                </div>
+                <div>{event.title}</div>
+                <div>{event.resourceTitle ?? event.resourceId}</div>
+              </article>
+            ))}
+          </section>
+        )}
+      </Fragment>
+    ));
+  };
+
+  const renderSingle = (events) => {
+    const elements = [];
+
+    if (events.length > 0) {
+      events.forEach((event) => {
+        if (elements.length < 3) {
+          elements.push(
+            <div
+              key={event.id}
+              className={elements.length === 0 ? "single--now" : "single--next"}
+            >
+              <div className="single--meta">
+                {renderTimeOfDay(event.startTime)}
+                {" - "}
+                {renderTimeOfDay(event.endTime)}
+              </div>
+              {event?.title ?? resourceUnavailableText ?? (
+                <FormattedMessage
+                  id="unavailable"
+                  defaultMessage="Unavailable"
+                />
+              )}
+            </div>
+          );
+        }
+      });
+    }
+
+    return elements.concat();
+  };
+
   return (
     <>
-      <ThemeStyles name="template-book-review" css={slide?.themeData?.css} />
+      <ThemeStyles name="template-calendar" css={slide?.themeData?.css} />
       <IntlProvider messages={translations} locale="da" defaultLocale="da">
-        <div className={classes}>
-          <div className="grid-container-title-date">
-            <div className="grid-item">{title}</div>
-            <div className="grid-item-end">
-              {currentDate &&
-                capitalize(dayjs().locale(localeDa).format("LLLL"))}
-            </div>
-          </div>
-          <div className="grid-container">
-            <div className="grid-item" key={1}>
-              <FormattedMessage id="what" defaultMessage="what" />
-            </div>
-            <div className="grid-item" key={2}>
-              <FormattedMessage id="when" defaultMessage="when" />
-            </div>
-            <div className="grid-item" key={3}>
-              <FormattedMessage id="where" defaultMessage="where" />
-            </div>
-            {feedData?.length > 0 &&
-              getSortedEvents(feedData).map((entry) => (
-                <Fragment key={entry.id}>
-                  <div className="grid-item">{entry.title}</div>
-                  <div className="grid-item">
-                    {dayjs(entry.startTime * 1000)
-                      .locale(localeDa)
-                      .format("LT")}
+        <div className={classes.join(" ")} style={rootStyle}>
+          {layout === "single" && (
+            <>
+              <div className="single--header">
+                {title}
+                {subTitle && (
+                  <>
+                    <br />
+                    <div className="single--meta">{subTitle}</div>
+                  </>
+                )}
+              </div>
+              <div className="single--content">
+                {feedData?.length === 0 && (
+                  <div className="single--available">
+                    {resourceAvailableText}
                   </div>
-                  <div className="grid-item">
-                    {entry.resourceTitle ?? entry.resourceId ?? ""}
-                  </div>
-                </Fragment>
-              ))}
-          </div>
+                )}
+                {feedData?.length > 0 && renderSingle(feedData)}
+              </div>
+            </>
+          )}
+          {layout === "multipleDays" && (
+            <>
+              <h1 className="multiple-days--header">{title}</h1>
+              <main className="multiple-days--content">
+                {feedData?.length > 0 &&
+                  renderDays(groupEventsByDays(feedData))}
+              </main>
+              {footerText && (
+                <footer className="multiple-days--footer">
+                  <span className="multiple-days--footer-text">
+                    {footerText}
+                  </span>
+                </footer>
+              )}
+            </>
+          )}
+          {layout === "multiple" && (
+            <>
+              <div className="grid-container-title-date">
+                <div className="grid-item">{title}</div>
+                <div className="grid-item-end">
+                  {currentDate &&
+                    capitalize(dayjs().locale(localeDa).format("LLLL"))}
+                </div>
+              </div>
+              <div className="grid-container">
+                {displayHeaders !== false && (
+                  <>
+                    <div className="grid-item header" key={1}>
+                      <FormattedMessage id="what" defaultMessage="what" />
+                    </div>
+                    <div className="grid-item header" key={2}>
+                      <FormattedMessage id="when" defaultMessage="when" />
+                    </div>
+                    <div className="grid-item header" key={3}>
+                      <FormattedMessage id="where" defaultMessage="where" />
+                    </div>
+                  </>
+                )}
+                {feedData?.length > 0 &&
+                  getSortedEvents(feedData).map((entry) => (
+                    <Fragment key={entry.id}>
+                      <div className="grid-item">{entry.title}</div>
+                      <div className="grid-item">
+                        {dayjs(entry.startTime * 1000)
+                          .locale(localeDa)
+                          .format("LT")}
+                      </div>
+                      <div className="grid-item">
+                        {entry.resourceTitle ?? entry.resourceId ?? ""}
+                      </div>
+                    </Fragment>
+                  ))}
+              </div>
+            </>
+          )}
         </div>
       </IntlProvider>
     </>
@@ -134,19 +283,31 @@ Calendar.propTypes = {
     themeData: PropTypes.shape({
       css: PropTypes.string,
     }),
-  }).isRequired,
-  content: PropTypes.shape({
-    events: PropTypes.arrayOf(
+    mediaData: PropTypes.objectOf(PropTypes.any),
+    feedData: PropTypes.arrayOf(
       PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        startTime: PropTypes.string.isRequired,
-        endTime: PropTypes.string.isRequired,
-        resourceTitle: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string,
+        startTime: PropTypes.number.isRequired,
+        endTime: PropTypes.number,
+        resourceTitle: PropTypes.string,
+        resourceId: PropTypes.string,
       })
     ),
-    title: PropTypes.string.isRequired,
-    backgroundColor: PropTypes.string.isRequired,
+  }).isRequired,
+  content: PropTypes.shape({
+    layout: PropTypes.string,
+    title: PropTypes.string,
+    subTitle: PropTypes.string,
+    colorize: PropTypes.bool,
+    colorizeColorClass: PropTypes.string,
+    backgroundColor: PropTypes.string,
     hasDateAndTime: PropTypes.bool,
+    image: PropTypes.arrayOf(PropTypes.string),
+    displayHeaders: PropTypes.bool,
+    footerText: PropTypes.string,
+    resourceAvailableText: PropTypes.string,
+    resourceUnavailableText: PropTypes.string,
   }).isRequired,
 };
 
