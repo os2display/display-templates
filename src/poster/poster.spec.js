15d17444929e17c40cc51be1504e84e766f080d8
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-03-02 07:55:23 +0100

    AR-769: Fixed test

--- src/poster/poster.spec.js
+++ src/poster/poster.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow, configure } from "enzyme";
+import { configure, mount } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import slides from "../slides";
 import Poster from "./poster";
@@ -8,13 +8,13 @@ configure({ adapter: new Adapter() });
 
 test("Test that poster loads", () => {
   const slide = slides[4];
-  const wrapper = shallow(
+  const wrapper = mount(
     <Poster run={new Date().toISOString()} slide={slide} content={slide.content} slideDone={() => {}} />
   );
 
   expect(wrapper.find(".image-area").get(0).props.style.backgroundImage).toBe(
     'url("/fixtures/images/mountain1.jpeg")'
   );
-  expect(wrapper.find("h1").text()).toContain("Havnerundfart med MS TUNØ");
+  expect(wrapper.find("h1").text()).toContain("Lorem ipsum");
   expect(wrapper.find(".ticket").text()).toContain("75-150 kr.");
 });
