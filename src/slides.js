982f6018f6ccbb5ed6f93bfc73197ecb12d104dd
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2023-02-28 13:57:51 +0100

    AR-931: Fixed header issue with font-size

--- src/slides.js
+++ src/slides.js
@@ -1126,6 +1126,31 @@ const slides = [
       fontSize: "font-size-m",
     },
   },
+  {
+    id: "slide18-image-text-font-sizes",
+    type: "image-text",
+    themeFile: "themes/dokk1.css",
+    mediaData: {
+      "/v1/media/00000000000000000000000001": {
+        assets: {
+          uri: "/fixtures/images/mountain1.jpeg",
+        },
+      },
+    },
+    content: {
+      duration: 5000,
+      title: "Slide 1",
+      text: "<h1>Fisk</h1><h2>Hest</h2><h3>Ræv</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      image: ["/v1/media/00000000000000000000000001"],
+      boxAlign: "right",
+      boxMargin: false,
+      shadow: true,
+      separator: true,
+      halfSize: true,
+      reversed: false,
+      fontSize: "font-size-xs",
+    },
+  },
 ];
 
 export default slides;
