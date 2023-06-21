803e8088f469ab50a07e74d097e8c3102fd85c3e
Sine Jespersen <sinejespersen@gmail.com>
2023-06-21 13:10:13 +0200

    add templates to screen template test

--- cypress/e2e/ScreenTemplates.cy.js
+++ cypress/e2e/ScreenTemplates.cy.js
@@ -1,6 +1,38 @@
 describe("template spec", () => {
-  it("passes", () => {
+  it("Two split screen template", () => {
     cy.visit("/two-split");
-    cy.wait(2000);
+    cy.get(".grid-index")
+      .should("exist")
+      .should("have.css", "grid-template-areas")
+      .should("include", "a b");
+    cy.get(".grid-element")
+      .eq(0)
+      .should("exist")
+      .should("have.css", "grid-area")
+      .should("include", "a / a / a / a");
+    cy.get(".grid-element")
+      .eq(1)
+      .should("exist")
+      .should("have.css", "grid-area")
+      .should("include", "b / b / b / b");
+  });
+
+  it("Two split vertical screen template", () => {
+    cy.visit("/two-split-vertical");
+    cy.get(".grid-index")
+      .should("exist")
+      .should("have.css", "grid-template-areas")
+      .should("include", '"a" "b" "c" "d" "e"');
+
+    cy.get(".grid-element")
+      .eq(0)
+      .should("exist")
+      .should("have.css", "grid-area")
+      .should("include", "a / a / c / c");
+    cy.get(".grid-element")
+      .eq(1)
+      .should("exist")
+      .should("have.css", "grid-area")
+      .should("include", "d / d / e / e");
   });
 });
