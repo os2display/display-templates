a7b9d8ad7396c1622efe38ce8393afe9a06e653a
Sine Jespersen <sinejespersen@gmail.com>
2023-06-21 10:20:26 +0200

    create screen template/update grid-generator

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
-import { createGridArea, createGrid } from "os2display-grid-generator";
+import {
+  createGridArea,
+  createGrid,
+  determineGridArea,
+} from "os2display-grid-generator";
 import {
   BrowserRouter,
   Link,
@@ -267,6 +271,144 @@ export const Overview = () => {
           </li>
         ))}
       </ul>
+      <h2>Lav en skærm</h2>
+      <Link to={`/create-screen`}>Lav en skærmtemplate</Link>
+    </>
+  );
+};
+
+const CreateScreenTemplate = () => {
+  const [rows, setRows] = useState(2);
+  const [columns, setColumns] = useState(2);
+  const [displayJSON, setDisplayJSON] = useState(false);
+  const [regions, setRegions] = useState([]);
+
+  /** Generate JSON from regions. */
+  function getRegionsForJson() {
+    return regions.map(({ gridArea }) => {
+      return {
+        id: "generate an ulid",
+        title: "gives til block a great name",
+        gridArea,
+      };
+    });
+  }
+
+  const [data, setData] = useState({});
+  const [areasToBeMerged, setAreasToBeMerged] = useState("");
+  const gridTemplateAreas = {
+    gridTemplateAreas: createGrid(Number(columns), Number(rows)),
+  };
+
+  useEffect(() => {
+    const alphabet = determineGridArea(columns, rows);
+    const regionsFromRowsColumns = [];
+    Array.from(Array(rows * columns).keys()).forEach((e, index) => {
+      regionsFromRowsColumns.push({
+        gridArea: [alphabet[index]],
+        title: alphabet[index],
+      });
+    });
+    setRegions(regionsFromRowsColumns);
+  }, [rows, columns]);
+
+  useEffect(() => {
+    setData({
+      title: "Give template a title",
+      id: "Generate ulid for template",
+      grid: {
+        rows,
+        columns,
+      },
+      regions: getRegionsForJson(),
+    });
+  }, [regions]);
+
+  /** Merge grid areas from input. */
+  function mergeGridAreas() {
+    const regionsCopy = [...regions];
+    areasToBeMerged.split(",").forEach((element) => {
+      const index = regionsCopy.findIndex((e) => e.title === element);
+      regionsCopy.splice(index, 1);
+    });
+
+    const lastGridCharacter =
+      areasToBeMerged.split(",")[areasToBeMerged.split(",").length - 1];
+    const firstGridCharacter = areasToBeMerged.split(",")[0];
+    regionsCopy.push({
+      gridArea: [`${firstGridCharacter}`, `${lastGridCharacter}`],
+      title: `${firstGridCharacter} / ${firstGridCharacter} / ${lastGridCharacter} / ${lastGridCharacter}`,
+    });
+    setRegions(regionsCopy);
+  }
+
+  return (
+    <>
+      <div style={gridTemplateAreas} className="grid-index">
+        {regions.map(({ gridArea, title }) => (
+          <div
+            className="grid-element"
+            style={{ gridArea: createGridArea(gridArea) }}
+          >
+            {title}
+            <br />
+            gridarea:
+            <br />
+            <div>
+              {gridArea.map((area) => (
+                <div>{area}</div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="inputs-container">
+        <label htmlFor="rows">
+          Rows
+          <input
+            id="rows"
+            min="1"
+            max="50" // I dunnoman
+            value={rows}
+            onChange={(e) => setRows(e.target.value)}
+            type="number"
+          />
+        </label>
+        <label htmlFor="columns">
+          Columns
+          <input
+            id="columns"
+            min="1"
+            max="50" // I dunnoman
+            onChange={(e) => setColumns(e.target.value)}
+            value={columns}
+            type="number"
+          />
+        </label>
+        <label htmlFor="merge">
+          Merge grid areas (seperate with comma)
+          <input
+            id="merge"
+            onChange={(e) => setAreasToBeMerged(e.target.value)}
+            type="text"
+          />
+        </label>
+        <button type="button" onClick={() => mergeGridAreas()}>
+          Merge grid areas
+        </button>
+        <button type="button" onClick={() => setDisplayJSON(!displayJSON)}>
+          {displayJSON && <>Remove json from screen</>}
+          {!displayJSON && <> Display json</>}
+        </button>
+      </div>
+      {displayJSON && (
+        <pre
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(data, null, 2),
+          }}
+        />
+      )}
     </>
   );
 };
@@ -278,6 +420,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path=":id" element={<DisplayElement />} />
+      <Route path="create-screen" element={<CreateScreenTemplate />} />
       <Route index element={<Overview />} />
     </Routes>
   </BrowserRouter>
